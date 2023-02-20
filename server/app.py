deeee5ea2feb565be86f42a2e9547e5a98636ef9
yummybomb <nickchen12345@gmail.com>
2023-02-19 22:19:57 -0500

    working backend questions for spanish

--- server/app.py
+++ server/app.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 from firebase import firebase
 import json
  
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../')
 fb_app = firebase.FirebaseApplication('https://lingulink-default-rtdb.firebaseio.com/', None)
 result = fb_app.get('/', None)
-print(result["Questions"][0])
-@app.route('/levelpick/int:<data>')
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+@app.route('/levelpick/<int:data>')
 def levelpick(data):
     result = fb_app.get('/', None)
     return result["Questions"][data]
@@ -14,4 +16,4 @@ def levelpick(data):
 
 
 
-#app.run(port=5000, debug=True)
+app.run(port=5000, debug=True)
